411b3ebc434e5881c58a6db9dc611147d933dbbc
Vishnu Vardhan Vankayalapati <v_vankalaya@uncg.edu>
2024-08-19 05:17:29 +0530

    error update

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const path = require("path");
 const express = require("express");
 
 const db = require("./data/database");
-
 const authRoutes = require("./routes/auth.routes");
 
 const app = express();
@@ -12,6 +11,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
 
 app.use(authRoutes);
 
--- data/database.js
+++ data/database.js
@@ -1,26 +1,23 @@
 const mongodb = require('mongodb');
-const { get } = require('../routes/auth.routes');
 
 const MongoClient = mongodb.MongoClient;
 
 let database;
 
 async function connectToDatabase() {
-    const client = await MongoClient.connect('mongodb://localhost:27017');
-    database = client.db('online-shop');
+  const client = await MongoClient.connect('mongodb://127.0.0.1:27017');
+  database = client.db('online-shop');
 }
 
-
 function getDb() {
-    if (!database) {
-        throw new Error('You must connect first!');
-    }
+  if (!database) {
+    throw new Error('You must connect first!');
+  }
 
-    return database;
+  return database;
 }
 
 module.exports = {
-    connectToDatabase: connectToDatabase,
-    getDb: getDb
-}
-
+  connectToDatabase: connectToDatabase,
+  getDb: getDb
+};
