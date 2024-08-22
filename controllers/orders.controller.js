f81c92c69e53c7dc96ee438267f8c8bb9086aba4
Vishnu Vardhan Vankayalapati <v_vankalaya@uncg.edu>
2024-08-22 23:57:25 +0530

    introduced Stripe API for the payments

--- controllers/orders.controller.js
+++ controllers/orders.controller.js
@@ -1,5 +1,7 @@
 const Order = require('../models/order.model');
 const User = require('../models/user.model');
+const stripe = require('stripe')('sk_test_51Pqe9IHsh2xbhQsjQYr5YEliBweZEphD583U9n2wvPajNwMV9HRjxgdkTMW37JhfSWr32jBzsHG2Bh0UQt9tT7jT00ra72LNJh');
+
 
 async function getOrders(req, res) {
   try {
@@ -33,10 +35,40 @@ async function addOrder(req, res, next) {
 
   req.session.cart = null;
 
+  const session = await stripe.checkout.sessions.create({
+    line_items: cart.items.map( function (item) {
+      return {
+        price_data: {
+          currency: 'usd',
+          product_data: {
+            name: item.product.title,
+          },
+          unit_amount: +item.product.price.toFixed(2) * 100
+        },
+        quantity: item.quantity,
+      }
+    }),
+    mode: 'payment',
+    success_url: 'http://localhost:3000/orders/success',
+    cancel_url: 'http://localhost:3000/orders/failure',
+  });
+
+  res.redirect(303, session.url);
+
   res.redirect('/orders');
+}
+
+function getSuccess(req, res) {
+  res.render('customer/orders/success');
+}
+
+function getFailure(req, res) {
+  res.render('customer/orders/failure');
 }
 
 module.exports = {
   addOrder: addOrder,
   getOrders: getOrders,
+  getSuccess: getSuccess,
+  getFailure: getFailure
 };
